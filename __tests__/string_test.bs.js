7f2c1a389fb76dc58b88e520b91adab3df4ba240
glennsl <glenn@slotte.net>
2017-12-28 19:01:30 +0100

    add tests for String

---
+++ __tests__/string_test.bs.js
@@ -0,0 +1,299 @@
+'use strict';
+
+var Jest   = require("bs-jest/src/jest.js");
+var Curry  = require("bs-platform/lib/js/curry.js");
+var Rebase = require("../src/rebase.bs.js");
+
+Jest.testAll("length", /* :: */[
+      /* tuple */[
+        "foo",
+        3
+      ],
+      /* :: */[
+        /* tuple */[
+          "m\xc3\xb8\xc3\xb8",
+          5
+        ],
+        /* :: */[
+          /* tuple */[
+            "møø",
+            3
+          ],
+          /* [] */0
+        ]
+      ]
+    ], (function (param) {
+        return Curry._2(Jest.Expect[/* Operators */24][/* = */5], Jest.Expect[/* expect */0](Rebase.$$String[/* length */0](param[0])), param[1]);
+      }));
+
+Jest.test("concat", (function () {
+        return Curry._2(Jest.Expect[/* Operators */24][/* == */0], Jest.Expect[/* expect */0](Rebase.$$String[/* concat */1]("b", "a")), "ab");
+      }));
+
+Jest.testAll("includes", /* :: */[
+      /* tuple */[
+        "nana",
+        /* true */1
+      ],
+      /* :: */[
+        /* tuple */[
+          "nanas",
+          /* false */0
+        ],
+        /* [] */0
+      ]
+    ], (function (param) {
+        return Curry._2(Jest.Expect[/* Operators */24][/* == */0], Jest.Expect[/* expect */0](Rebase.$$String[/* includes */2](param[0], "banana")), param[1]);
+      }));
+
+Jest.testAll("startsWith", /* :: */[
+      /* tuple */[
+        "ba",
+        /* true */1
+      ],
+      /* :: */[
+        /* tuple */[
+          "na",
+          /* false */0
+        ],
+        /* [] */0
+      ]
+    ], (function (param) {
+        return Curry._2(Jest.Expect[/* Operators */24][/* == */0], Jest.Expect[/* expect */0](Rebase.$$String[/* startsWith */3](param[0], "banana")), param[1]);
+      }));
+
+Jest.testAll("endsWith", /* :: */[
+      /* tuple */[
+        "ba",
+        /* false */0
+      ],
+      /* :: */[
+        /* tuple */[
+          "na",
+          /* true */1
+        ],
+        /* [] */0
+      ]
+    ], (function (param) {
+        return Curry._2(Jest.Expect[/* Operators */24][/* == */0], Jest.Expect[/* expect */0](Rebase.$$String[/* endsWith */4](param[0], "banana")), param[1]);
+      }));
+
+Jest.testAll("isEmpty", /* :: */[
+      /* tuple */[
+        "",
+        /* true */1
+      ],
+      /* :: */[
+        /* tuple */[
+          "foo",
+          /* false */0
+        ],
+        /* :: */[
+          /* tuple */[
+            "\t",
+            /* true */1
+          ],
+          /* :: */[
+            /* tuple */[
+              "\n",
+              /* true */1
+            ],
+            /* :: */[
+              /* tuple */[
+                "\r",
+                /* true */1
+              ],
+              /* :: */[
+                /* tuple */[
+                  " ",
+                  /* true */1
+                ],
+                /* :: */[
+                  /* tuple */[
+                    "\t\r\n ",
+                    /* true */1
+                  ],
+                  /* [] */0
+                ]
+              ]
+            ]
+          ]
+        ]
+      ]
+    ], (function (param) {
+        return Curry._2(Jest.Expect[/* Operators */24][/* == */0], Jest.Expect[/* expect */0](Rebase.$$String[/* isEmpty */5](param[0])), param[1]);
+      }));
+
+Jest.testAll("padStart", /* :: */[
+      /* tuple */[
+        6,
+        "na",
+        "banana"
+      ],
+      /* :: */[
+        /* tuple */[
+          9,
+          "na",
+          "nanbanana"
+        ],
+        /* :: */[
+          /* tuple */[
+            10,
+            "na",
+            "nanabanana"
+          ],
+          /* :: */[
+            /* tuple */[
+              -10,
+              "na",
+              "banana"
+            ],
+            /* [] */0
+          ]
+        ]
+      ]
+    ], (function (param) {
+        return Curry._2(Jest.Expect[/* Operators */24][/* == */0], Jest.Expect[/* expect */0](Rebase.$$String[/* padStart */6](param[0], param[1], "banana")), param[2]);
+      }));
+
+Jest.testAll("padEnd", /* :: */[
+      /* tuple */[
+        6,
+        "na",
+        "banana"
+      ],
+      /* :: */[
+        /* tuple */[
+          9,
+          "na",
+          "banananan"
+        ],
+        /* :: */[
+          /* tuple */[
+            10,
+            "na",
+            "banananana"
+          ],
+          /* :: */[
+            /* tuple */[
+              -10,
+              "na",
+              "banana"
+            ],
+            /* [] */0
+          ]
+        ]
+      ]
+    ], (function (param) {
+        return Curry._2(Jest.Expect[/* Operators */24][/* == */0], Jest.Expect[/* expect */0](Rebase.$$String[/* padEnd */7](param[0], param[1], "banana")), param[2]);
+      }));
+
+Jest.testAll("trim", /* :: */[
+      /* tuple */[
+        "  a",
+        "a"
+      ],
+      /* :: */[
+        /* tuple */[
+          "a  ",
+          "a"
+        ],
+        /* :: */[
+          /* tuple */[
+            "  a  ",
+            "a"
+          ],
+          /* :: */[
+            /* tuple */[
+              "\t a \t ",
+              "a"
+            ],
+            /* :: */[
+              /* tuple */[
+                "\n a \r ",
+                "a"
+              ],
+              /* [] */0
+            ]
+          ]
+        ]
+      ]
+    ], (function (param) {
+        return Curry._2(Jest.Expect[/* Operators */24][/* == */0], Jest.Expect[/* expect */0](Rebase.$$String[/* trim */8](param[0])), param[1]);
+      }));
+
+Jest.testAll("sum", /* :: */[
+      /* tuple */[
+        0,
+        0,
+        ""
+      ],
+      /* :: */[
+        /* tuple */[
+          1,
+          0,
+          ""
+        ],
+        /* :: */[
+          /* tuple */[
+            0,
+            1,
+            "b"
+          ],
+          /* :: */[
+            /* tuple */[
+              2,
+              2,
+              "na"
+            ],
+            /* :: */[
+              /* tuple */[
+                3,
+                4,
+                "ana"
+              ],
+              /* :: */[
+                /* tuple */[
+                  3,
+                  8,
+                  "ana"
+                ],
+                /* :: */[
+                  /* tuple */[
+                    7,
+                    1,
+                    ""
+                  ],
+                  /* :: */[
+                    /* tuple */[
+                      0,
+                      -1,
+                      ""
+                    ],
+                    /* :: */[
+                      /* tuple */[
+                        -1,
+                        -1,
+                        ""
+                      ],
+                      /* :: */[
+                        /* tuple */[
+                          -1,
+                          1,
+                          "a"
+                        ],
+                        /* [] */0
+                      ]
+                    ]
+                  ]
+                ]
+              ]
+            ]
+          ]
+        ]
+      ]
+    ], (function (param) {
+        return Curry._2(Jest.Expect[/* Operators */24][/* == */0], Jest.Expect[/* expect */0](Rebase.$$String[/* sub */9](param[0], param[1], "banana")), param[2]);
+      }));
+
+/*  Not a pure module */
---
+++ __tests__/string_test.re
@@ -0,0 +1,73 @@
+open Jest;
+open Expect;
+open! Expect.Operators;
+open Rebase;
+
+testAll("length", [
+    ("foo", 3),
+    ("møø", 5),
+    ({js|møø|js}, 3),
+  ], ((input, expected)) => expect(String.length(input)) == expected);
+
+test("concat", () =>
+  expect("a" |> String.concat("b")) === "ab");
+
+testAll("includes", [
+  ("nana", true),
+  ("nanas", false),
+], ((input, expected)) => expect("banana" |> String.includes(input)) === expected);
+
+testAll("startsWith", [
+  ("ba", true),
+  ("na", false),
+], ((input, expected)) => expect("banana" |> String.startsWith(input)) === expected);
+
+testAll("endsWith", [
+  ("ba", false),
+  ("na", true),
+], ((input, expected)) => expect("banana" |> String.endsWith(input)) === expected);
+
+testAll("isEmpty", [
+  ("", true),
+  ("foo", false),
+  ("\t", true),
+  ("\n", true),
+  ("\r", true),
+  (" ", true),
+  ("\t\r\n ", true),
+], ((input, expected)) => expect(String.isEmpty(input)) === expected);
+
+testAll("padStart", [
+  (6, "na", "banana"),
+  (9, "na", "nanbanana"),
+  (10, "na", "nanabanana"),
+  (-10, "na", "banana"),
+], ((count, pad, expected)) => expect("banana" |> String.padStart(count, pad)) === expected);
+
+testAll("padEnd", [
+  (6, "na", "banana"),
+  (9, "na", "banananan"),
+  (10, "na", "banananana"),
+  (-10, "na", "banana"),
+], ((count, pad, expected)) => expect("banana" |> String.padEnd(count, pad)) === expected);
+
+testAll("trim", [
+  ("  a", "a"),
+  ("a  ", "a"),
+  ("  a  ", "a"),
+  ("\t a \t ", "a"),
+  ("\n a \r ", "a"),
+], ((input, expected)) => expect(String.trim(input)) === expected);
+
+testAll("sum", [
+  (0, 0, ""),
+  (1, 0, ""),
+  (0, 1, "b"),
+  (2, 2, "na"),
+  (3, 4, "ana"),
+  (3, 8, "ana"),
+  (7, 1, ""),
+  (0, -1, ""),
+  (-1, -1, ""),
+  (-1, 1, "a"),
+], ((from, length, expected)) => expect("banana" |> String.sub(~from, ~length)) === expected);
